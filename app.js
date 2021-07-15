f735f1785422d47a54f66d7d9bf781dbc523a9a0
Kamran Wahab <33278325+kamranwahab@users.noreply.github.com>
2021-07-15 15:52:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect("mongodb+srv://Kamran:06April1997@terminal.ec7ag.mongodb.net/Test", {
+  .connect("mongodb+srv://terminal:terminal@terminal.oujzb.mongodb.net/Terminal", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
